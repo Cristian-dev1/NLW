7a677a1951875cdb804dcff4baea780d3ac0d25d
Cristian Eduardo <Cristian_edu2015@outlook.com>
2020-06-06 20:14:39 -0300

    Primeiro commit

---
+++ public/scripts/intro.js
@@ -0,0 +1,67 @@
+// comments
+// document.write('hello')
+
+// variaveis, tipos de dados
+// let myvar = "he"
+// const myconst = 'He'
+
+// document.write(myconst + myvar) 
+
+// string
+// "isso é uma string"
+// 'Isso é uma string'
+// `Isso é uma string`
+
+// number
+// const n1= 1
+// const n2 12
+// document.write(n1+n2)
+ 
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+// altura; "1,80mm"
+// idade: 24,
+//solteiro: true,
+//correr(){
+//   document.write("executar uma grande lófica de correr")
+//}
+//}
+
+// pessoa.correr()
+
+//array - vetores
+// const colecaoDeBolinhas = ["blue, "gren", 1, (name: "My name")]
+
+//document.write(colecaoDeBolinhas[3].name)
+
+// funções - FUncionalides- atalhos - reuso do código
+
+// // registrar função
+// function sayMyName(name {
+//document.write(name)
+//})
+
+// // executar
+// sayMyName("douglas")
+// sayMyName("Valeska")
+// sayMyName("Kyam")
+
+//Condicionais
+
+//const notafinal = 7
+
+// if ( notafinal < 5) {
+//document.write("Reprovado")
+//} else {
+//  document.write("Aprovado")
+// }
+
+// loop repetições
+// for (i = 0; i < 10: i++) {
+// document.write('<p>${1}</p>')
+//}
